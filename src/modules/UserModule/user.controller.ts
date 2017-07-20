6f358e75cc63976a4e2c46f8833b911db116d6f3
Angel D. Munoz <scythes.lol@gmail.com>
2017-07-20 00:12:47 -0500

    add a sample user controller

---
+++ src/modules/UserModule/user.controller.ts
@@ -0,0 +1,46 @@
+
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Req, Res } from '@nestjs/common';
+import { Response } from "express";
+import { IUser, UserService } from "./utils";
+
+@Controller('users')
+export class UserController {
+  constructor(private $service: UserService) {
+    /**
+     * this is just a dummy user service
+     */
+    this.$service.find().then(users => console.log('Actual Users', users))
+      .catch(console.error);
+  }
+
+  @Post()
+  public async create( @Res() res: Response, @Body('user') user: IUser) {
+    const createdUser = await this.$service.create(user);
+    res.status(HttpStatus.CREATED).json(createdUser)
+  }
+
+  @Get()
+  public async find( @Req() req, @Res() res: Response) {
+    const users = await this.$service.find();
+    res.status(HttpStatus.OK).json(users);
+  }
+
+  @Get('/:id')
+  public async findOne( @Req() req, @Res() res: Response, @Param('id') id) {
+    id = parseInt(id, 10) || id.toLowerCase();
+    const user = await this.$service.findOne(id);
+    res.status(HttpStatus.OK).json(user);
+  }
+
+  @Put('/:id')
+  public async update( @Req() req, @Res() res: Response, @Body('user') user: IUser, @Param('id') id) {
+    const updatedUser = await this.$service.update(id, user);
+    res.status(HttpStatus.OK).json(updatedUser)
+  }
+
+  @Delete('/:id')
+  public async deleteEmployee( @Req() req, @Res() res: Response, @Param('id') id) {
+    const deletedEmployee = await this.$service.destroy(id);
+    res.status(HttpStatus.OK).json(deletedEmployee);
+  }
+}
